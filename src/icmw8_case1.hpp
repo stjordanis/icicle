4f11df6a5d2fc6a78ee904eba7d0236a14e8517b
Sylwester Arabas <slayoo@igf.fuw.edu.pl>
2015-05-07 16:54:23 +0200

    switching to double precision

--- src/icmw8_case1.hpp
+++ src/icmw8_case1.hpp
@@ -18,7 +18,7 @@
 // 8th ICMW case 1 by Wojciech Grabowski)
 namespace icmw8_case1
 {
-  using real_t = float; //double;
+  using real_t = double;
 
   namespace hydrostatic = libcloudphxx::common::hydrostatic;
   namespace theta_std = libcloudphxx::common::theta_std;
